3b8bfe7d5fb33fe0a79aaf273c59381a98ebdfb6
aesacus <jayrbolton@gmail.com>
2012-09-21 17:22:34 -0700

    initialization

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+function dayDif(currentDate) {
+	var targetDay = new Date('January 01, 2013');
+	return Math.round((targetDay - currentDate)/(1000*60*60*24));
+}
+
+var total = dayDif(new Date('September 01, 2012'));
+var decTest = new Date('December 15, 2012');
+var remaining = dayDif(new Date()); // Number of days from now until target date (1.1.13)
+var percentage = (100 * (total - remaining)) / total
+
+$(document).ready(function() {
+	$('.bar').css('width', String(percentage) + '%');
+});
